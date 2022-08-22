5b3008225190ada951274f93bce809257f598290
Code Sandbox <hello@codesandbox.io>
2022-08-22 03:31:06 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.9376ed19bcaf9c38a2f6cf757e6d5113.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "44864849ec14240d6712",
+    "url": "https://codemonkey-stack.github.io/csb-0rgedw/static/css/main.f1f3bfb0.chunk.css"
+  },
+  {
+    "revision": "44864849ec14240d6712",
+    "url": "https://codemonkey-stack.github.io/csb-0rgedw/static/js/main.23f97082.chunk.js"
+  },
+  {
+    "revision": "6491b35f32ceb405bcfd",
+    "url": "https://codemonkey-stack.github.io/csb-0rgedw/static/js/runtime~main.ef755457.js"
+  },
+  {
+    "revision": "f01ce4880824226428e6",
+    "url": "https://codemonkey-stack.github.io/csb-0rgedw/static/js/2.c398c6cf.chunk.js"
+  },
+  {
+    "revision": "d0b6527e0e58d42b77bf8655306a7039",
+    "url": "https://codemonkey-stack.github.io/csb-0rgedw/static/media/logo.d0b6527e.png"
+  },
+  {
+    "revision": "74c39610d3a214b831dd203dec9273e6",
+    "url": "https://codemonkey-stack.github.io/csb-0rgedw/index.html"
+  }
+];
